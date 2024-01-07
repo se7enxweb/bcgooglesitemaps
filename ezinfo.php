858d9bae01c18d4a210028c199ada81d8d984db6
7x <info@se7enx.com>
2024-01-07 06:53:33 -0800

    Fork cronjob tool for inclusion in ezpublish (From 7x)

--- ezinfo.php
+++ ezinfo.php
@@ -2,7 +2,7 @@
 /**
  * File containing the bcgooglesitemaps copyright information file
  *
- * @copyright Copyright (C) 1999 - 2024 Brookins Consutling, 2008 all2e GmbH and 2008 MEDIATA Communications GmbH. All rights reserved.
+ * @copyright Copyright (C) 1999 - 2024 7x, Brookins Consutling, 2008 all2e GmbH and 2008 MEDIATA Communications GmbH. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or Later Version)
  * @version //autogentag//
  * @package bcgooglesitemaps
@@ -12,11 +12,11 @@ class bcGoogleSitemapsInfo
 {
     public static function info()
     {
-        return array( 'Name' => "<a href='https://github.com/brookinsconsulting/bcgooglesitemaps'>BC Google Sitemaps</a>",
+        return array( 'Name' => "<a href='https://github.com/se7enxweb/bcgooglesitemaps'>BC Google Sitemaps</a>",
                       'Version' => "1.1.6",
-                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='http://brookinsconsulting.com' title='Brookins Consulting'>Brookins Consulting</a>, 2008 <a href='http:www.all2e.com' title='all2e GmbH'>all2e GmbH</a> and 2008 <a href='http://www.mediata.net' title='MEDIATA Communications GmbH'>MEDIATA Communications GmbH</a>",
+                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a> and Brookins Consulting, 2008 <a href='http:www.all2e.com' title='all2e GmbH'>all2e GmbH</a> and 2008 <a href='http://www.mediata.net' title='MEDIATA Communications GmbH'>MEDIATA Communications GmbH</a>",
                       'License' => "GNU General Public License v2.0 (or Later Version)",
-                      'info_url' => "https://github.com/brookinsconsulting/bcgooglesitemaps"
+                      'info_url' => "https://packagist.org/packages/se7enxweb/bcgooglesitemaps"
                     );
     }
 }
